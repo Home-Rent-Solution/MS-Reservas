519b446cee9e2721d80d00c5a80d692f30a1f2b5
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-12 11:40:08 -0400

    se agrega un getmapping a la interfaz de client

--- src/main/java/com/HomeRentSolution/ms_reservas/client/EspecialidadClient.java
+++ src/main/java/com/HomeRentSolution/ms_reservas/client/EspecialidadClient.java
@@ -1,13 +1,19 @@
 package com.HomeRentSolution.ms_reservas.client;
 
+import com.HomeRentSolution.ms_reservas.dto.ReservaPropiedadDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @FeignClient(name = "ms-especialidades",
 url = "${ms.especialidades.url}")
 
 public interface EspecialidadClient {
+
+    @GetMapping("/api/propiedades") // El endpoint que exponga tu compañero
+    List<ReservaPropiedadDTO> obtenerTodas();
 
     @GetMapping("/api/especialidades/{id}")
     String obtenetPorId(@PathVariable Long id);
--- src/main/java/com/HomeRentSolution/ms_reservas/service/ReservaService.java
+++ src/main/java/com/HomeRentSolution/ms_reservas/service/ReservaService.java
@@ -1,6 +1,7 @@
 package com.HomeRentSolution.ms_reservas.service;
 
 import com.HomeRentSolution.ms_reservas.dto.ReservaClienteDTO;
+import com.HomeRentSolution.ms_reservas.dto.ReservaPropiedadDTO;
 import com.HomeRentSolution.ms_reservas.model.EstadoReserva;
 import com.HomeRentSolution.ms_reservas.model.Reserva;
 import com.HomeRentSolution.ms_reservas.repository.ReservaRepository;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -16,12 +19,26 @@ public class ReservaService {
 
     private final ReservaRepository reservaRepository;
 
+
+    public List<ReservaPropiedadDTO> buscarDisponiblesParaCalendario(LocalDateTime inicio, LocalDateTime fin) {
+        // 1. Traer TODAS las propiedades del micro de tu compañero vía Feign
+        List<ReservaPropiedadDTO> todas = propiedadClient.obtenerTodas();
+
+        // 2. Obtener de TU base de datos los IDs de las propiedades ocupadas
+        // Nota: Si tu DB usa Long y él Integer, solo hacemos un cast rápido
+        List<Integer> idsOcupados = reservaRepository.findIdsOcupadosEnRango(inicio, fin)
+                .stream()
+                .map(Long::intValue)
+                .collect(Collectors.toList());
+
+        // 3. Filtrar: "Deme las que NO están ocupadas"
+        return todas.stream()
+                .filter(p -> !idsOcupados.contains(p.getIdPropiedad()))
+                .collect(Collectors.toList());
+    }
+
     public ReservaClienteDTO crearReserva(Long idInquilino, Long idPropiedad,
                                           LocalDateTime inicio, LocalDateTime fin) {
-
-
-
-
 
         Reserva reserva = new Reserva();
         reserva.setFechaCreacion(LocalDateTime.now());
