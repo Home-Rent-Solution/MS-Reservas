798349d4f50386e15a9ad7c75836aa33a762f51b
Naoshinaps <n.paillallao.17@gmail.com>
2026-05-12 13:19:54 -0400

    cambio en variables de reserva adm dto

--- src/main/java/com/HomeRentSolution/ms_reservas/dto/ReservaAdmDTO.java
+++ src/main/java/com/HomeRentSolution/ms_reservas/dto/ReservaAdmDTO.java
@@ -3,16 +3,24 @@ package com.HomeRentSolution.ms_reservas.dto;
 import com.HomeRentSolution.ms_reservas.model.EstadoReserva;
 import lombok.Data;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class ReservaAdmDTO {
 
-    private Long id;
-    private LocalDateTime fechaCreacion;
-    private LocalDateTime fechaInicio;
-    private LocalDateTime fechaFin;
-    private EstadoReserva estado;
-    private Long idInquilino;
-    private String idPropiedad;
+    private Integer idReserva;
+    private Integer idPropiedad;
+    private Integer idInquilino;
+    private String nombreInquilino;
+    private String tituloPropiedad;
+    private String ubicacion;
+    private Propiedades.TipoPropiedad tipoPropiedad;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+    private BigDecimal montoTotal;
+    private String estado;
+    private LocalDateTime fechaReserva;
+    private LocalDateTime fechaLimitePago;
 }
