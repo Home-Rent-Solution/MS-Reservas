fab10c5f476fae1b7e5204f2564faa58b0f20c9d
catbarriosg <catbarriosg@duocuc.cl>
2026-06-21 19:27:20 -0400

    Limpia cliente Feign de pagos

--- src/main/java/com/HomeRentSolution/ms_reservas/client/PagosClient.java
+++ src/main/java/com/HomeRentSolution/ms_reservas/client/PagosClient.java
@@ -4,24 +4,12 @@ import com.HomeRentSolution.ms_reservas.dto.ms.PagosDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
-import java.math.BigDecimal;
-
 @FeignClient(name = "ms-pagos", url = "${ms.pagos.url}")
 public interface PagosClient {
-
-    @GetMapping("/api/pagos/{id}")
-    Boolean getPagoPorId(@PathVariable("id") Long idPago);
 
     @PostMapping("/api/v1/pagos")
     void crearPago(@RequestBody PagosDTO pagoRequest);
 
-    @PutMapping("/api/v1/pagos/{id}/cancelar")
-    void cancelarPago(
-            @PathVariable("id") Long idReserva,
-            @RequestParam String motivo,
-            @RequestParam BigDecimal montoReembolso);
-
     @PutMapping("/api/v1/pagos/{id}/confirmar")
     void confirmarPago(@PathVariable("id") Long idReserva);
-
 }
